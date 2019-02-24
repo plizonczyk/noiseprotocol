fab0b06d170c1f4fe0e2ea610ac28b82cb521b76
Piotr Lizonczyk <piotr.lizonczyk@shopspring.com>
2019-02-24 12:38:54 +0100

    Upgrade to Cryptography 2.5

--- noise/backends/default/keypairs.py
+++ noise/backends/default/keypairs.py
@@ -1,3 +1,4 @@
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519, x448
 
 from noise.exceptions import NoiseValueError
@@ -9,16 +10,16 @@ class KeyPair25519(KeyPair):
     def from_private_bytes(cls, private_bytes):
         if len(private_bytes) != 32:
             raise NoiseValueError('Invalid length of private_bytes! Should be 32')
-        private = x25519.X25519PrivateKey._from_private_bytes(private_bytes)
+        private = x25519.X25519PrivateKey.from_private_bytes(private_bytes)
         public = private.public_key()
-        return cls(private=private, public=public, public_bytes=public.public_bytes())
+        return cls(private=private, public=public, public_bytes=public.public_bytes(encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw))
 
     @classmethod
     def from_public_bytes(cls, public_bytes):
         if len(public_bytes) != 32:
             raise NoiseValueError('Invalid length of public_bytes! Should be 32')
         public = x25519.X25519PublicKey.from_public_bytes(public_bytes)
-        return cls(public=public, public_bytes=public.public_bytes())
+        return cls(public=public, public_bytes=public.public_bytes(encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw))
 
 
 class KeyPair448(KeyPair):
@@ -26,13 +27,13 @@ class KeyPair448(KeyPair):
     def from_private_bytes(cls, private_bytes):
         if len(private_bytes) != 56:
             raise NoiseValueError('Invalid length of private_bytes! Should be 56')
-        private = x448.X448PrivateKey._from_private_bytes(private_bytes)
+        private = x448.X448PrivateKey.from_private_bytes(private_bytes)
         public = private.public_key()
-        return cls(private=private, public=public, public_bytes=public.public_bytes())
+        return cls(private=private, public=public, public_bytes=public.public_bytes(encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw))
 
     @classmethod
     def from_public_bytes(cls, public_bytes):
         if len(public_bytes) != 56:
             raise NoiseValueError('Invalid length of private_bytes! Should be 56')
         public = x448.X448PublicKey.from_public_bytes(public_bytes)
-        return cls(public=public, public_bytes=public.public_bytes())
+        return cls(public=public, public_bytes=public.public_bytes(encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw))
